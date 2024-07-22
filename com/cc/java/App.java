63dbf6c0298dcdf4a5bf39a0aa698e239fe7dcac
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-07-22 12:28:43 +0200

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,40 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+     
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        Bird bird = new Bird();
+
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+
+        pollObj(bird);
+   
+
+        // HoneyBee ist abstrakt --> Fehler!
+        // HoneyBee bee = new HoneyBee();
+
+    }
+
+    // 2. Variante Polymorphie
+    private static void pollObj(HoneyBee bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+        output("------------");
+    }
+
+   
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+
+}
+
